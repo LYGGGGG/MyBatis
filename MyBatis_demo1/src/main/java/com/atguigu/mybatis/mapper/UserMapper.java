59793c2c5983353d0b0e3ded6625d6ddcdb1ec3c
YG <710431075@qq.com>
2022-12-28 20:14:31 +0800

    mapper接口

--- MyBatis_demo1/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
+++ MyBatis_demo1/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -5,6 +5,10 @@ package com.atguigu.mybatis.mapper;
 @create 2022/12/27   22:16
 */
 
+import com.atguigu.mybatis.pojo.User;
+
+import java.util.List;
+
 public interface UserMapper {
     /**
      * 添加用户信息
@@ -12,4 +16,10 @@ public interface UserMapper {
     int insertUser();
 
     int updateUser();
+
+    int deleteUser();
+
+    User queryUserById();
+
+    List<User> queryAllUser();
 }
