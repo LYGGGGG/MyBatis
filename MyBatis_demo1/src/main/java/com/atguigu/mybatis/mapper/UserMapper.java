1a93bef4003c71ed7fe25646881dda63870010fc
YG <710431075@qq.com>
2022-12-28 13:47:48 +0800

    .

--- MyBatis_demo1/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
+++ MyBatis_demo1/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -10,4 +10,6 @@ public interface UserMapper {
      * 添加用户信息
      */
     int insertUser();
+
+    int updateUser();
 }
--- MyBatis_demo1/src/test/java/com/atguigu/mybatis/test/MyBatisTest.java
+++ MyBatis_demo1/src/test/java/com/atguigu/mybatis/test/MyBatisTest.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class MyBatisTest {
+
     @Test
     public void testMyBatis() throws IOException {
         // 加载核心配置文件
@@ -25,13 +26,13 @@ public class MyBatisTest {
         // 通过核心配置文件所对应的字节输入流，创建工厂类SqlSessionFactory
         SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
         // 获取SqlSession对象。通过SqlSession对象操作的sql必须手动提交或回滚事务
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
         // 通过代理模式创建UserMapper接口的代理实现类对象
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         // 调用UserMapper接口中的方法，就可以根据UserMapper的全类名匹配元素文件，通过调用的方法名
         // 匹配映射文件中的SQL标签，并执行标签中的SQL语句
         int i = mapper.insertUser();
-        sqlSession.commit();
+//        sqlSession.commit();
         System.out.println("result: " + i);
     }
 }
