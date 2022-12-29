45c565c9559cf653413ef160047d1ed34097eab0
YG <710431075@qq.com>
2022-12-29 21:10:22 +0800

    模糊查询的mapper接口

--- MyBatis_demo2/src/main/java/com/atguigu/mybatis/mapper/SQLMapper.java
+++ MyBatis_demo2/src/main/java/com/atguigu/mybatis/mapper/SQLMapper.java
@@ -15,4 +15,19 @@ public interface SQLMapper {
      * 根据用户名进行模糊查询
      */
     List<User> getUserByLike(@Param("username") String username);
+
+    /**
+     * 批量删除
+     */
+    int deleteMore(@Param("ids") String ids);
+
+    /**
+     * 查询指定表中的数据
+     */
+    List<User> getUserByTable(@Param("tableName") String tableName);
+
+    /**
+     * 添加用户信息
+     */
+    void insertUser(User user);
 }
