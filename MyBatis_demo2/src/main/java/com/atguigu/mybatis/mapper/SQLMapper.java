3746182f91b1c82c84bde843d087df2992b1a2f3
YG <710431075@qq.com>
2022-12-29 17:55:53 +0800

    模糊查询的测试类

--- MyBatis_demo2/src/main/java/com/atguigu/mybatis/mapper/SQLMapper.java
+++ MyBatis_demo2/src/main/java/com/atguigu/mybatis/mapper/SQLMapper.java
@@ -5,5 +5,14 @@ package com.atguigu.mybatis.mapper;
 @create 2022/12/29   17:46
 */
 
+import com.atguigu.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface SQLMapper {
+    /**
+     * 根据用户名进行模糊查询
+     */
+    List<User> getUserByLike(@Param("username") String username);
 }
