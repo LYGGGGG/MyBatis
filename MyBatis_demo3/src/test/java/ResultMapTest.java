9c6859adaa3517d3ddcc50953797c1f9d32ee78d
YG <710431075@qq.com>
2022-12-30 13:29:09 +0800

    一对多 多对一 test文件

---
+++ MyBatis_demo3/src/test/java/ResultMapTest.java
@@ -0,0 +1,48 @@
+/*
+@author YG
+@create 2022/12/29   22:03
+*/
+
+import com.atguigu.mybatis.mapper.DeptMapper;
+import com.atguigu.mybatis.mapper.EmpMapper;
+import com.atguigu.mybatis.pojo.Dept;
+import com.atguigu.mybatis.pojo.Emp;
+import com.atguigu.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ResultMapTest {
+    @Test
+    public void testGetAllEmp() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        List<Emp> allEmp = mapper.getAllEmp();
+        allEmp.forEach(emp -> System.out.println(emp));
+    }
+
+    @Test
+    public void testGetEmpAndDept() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = mapper.getEmpAndDept(2);
+        System.out.println(emp);
+    }
+
+    @Test
+    public void testGetEmpAndDeptByStep() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = mapper.getEmpAndDeptByStep1(4);
+        System.out.println(emp);
+    }
+
+    @Test
+    public void testGetEmpAndDeptByStep2() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        DeptMapper mapper = sqlSession.getMapper(DeptMapper.class);
+        Dept dept = mapper.getEmpAndDeptByStep2(3);
+        System.out.println(dept);
+    }
+}
