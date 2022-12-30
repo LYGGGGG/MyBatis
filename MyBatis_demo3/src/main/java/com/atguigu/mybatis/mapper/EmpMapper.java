ce924929cfe2b78b3c464f1800acbb3ab97b1a2b
YG <710431075@qq.com>
2022-12-30 13:27:26 +0800

    一对多 多对一 员工的mapper接口

--- MyBatis_demo3/src/main/java/com/atguigu/mybatis/mapper/EmpMapper.java
+++ MyBatis_demo3/src/main/java/com/atguigu/mybatis/mapper/EmpMapper.java
@@ -5,5 +5,27 @@ package com.atguigu.mybatis.mapper;
 @create 2022/12/29   21:40
 */
 
+import com.atguigu.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface EmpMapper {
+    /**
+     * 查询所有的员工信息
+     */
+    List<Emp> getAllEmp();
+
+    /**
+     * 查询员工以及员工所对应的部门信息
+     */
+    Emp getEmpAndDept(@Param("eid") Integer eid);
+
+    /**
+     * 通过分步查询，查询员工以及员工所对应的部门信息
+     * 第一步：查询员工信息
+     */
+    Emp getEmpAndDeptByStep1(@Param("eid")Integer eid);
+
+
 }
